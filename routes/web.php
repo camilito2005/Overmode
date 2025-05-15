2b948e719a209b6bd49755b4f43b3869f24e908a
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-05-14 23:10:01 -0500

    feat: agregue las respectivas rutas para realizar la funcionalidades

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuariosController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/' , [UsuariosController::class, 'Index'])->name('index');
+
+Route::get('/usuarios', [UsuariosController::class, 'Form_html'])->name('usuarios.formulario');
+Route::post('/usuarios/Registrar', [UsuariosController::class, 'Registrar'])->name('usuarios.registrar');
+Route::get('/usuarios/listar', [UsuariosController::class, 'Listar'])->name('usuarios.listar');
+Route::put('/usuarios/actualizar/{id}', [UsuariosController::class, 'Actualizar'])->name('usuarios.actualizar');
+Route::delete('/usuarios/Eliminar/{id}', [UsuariosController::class, 'Eliminar'])->name('usuarios.eliminar');
+
