1d4615d23004ea69fb1b47537e4c2fbc262bb4b9
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-05-19 23:43:37 -0500

    feat: agregue las opciones al menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,18 +13,41 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Overmode</a>
+            <a class="navbar-brand" href="{{route('index')}}">Overmode</a>
+            @if (Auth::check())
+                <a class="navbar-brand">{{Auth::user()->nombre}}</a>
+                
+            @endif
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link active" href="{{route('index')}}">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Tienda</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Carrito</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{route('usuarios.listar')}}">Usuarios</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('usuarios.formulario') }}">Iniciar
-                            Sesión</a></li>
+                    @if (Auth::check())
+                        <li class="nav-item"><a class="nav-link active" href="{{ route('index') }}">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Carrito</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('usuarios.listar') }}">Usuarios</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('logout') }}">Cerrar Sesión</a></li>
+                        @if (Auth::user()->rol_id == 1)
+
+                            <li class="nav-item"><a class="nav-link" href="{{ route('perfil') }}">perfil</a></li>
+                            @if (Auth::user()->rol_id == 2)
+                                <li class="nav-item"><a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
+                                </li>
+                            @else
+                            @endif
+                        @else
+                            </li>
+
+                        @endif
+                    @else
+                        <li class="nav-item"><a class="nav-link" href="#">Tienda</a></li>
+                        <li class="nav-item"><a class="nav-link active" href="{{ route('index') }}">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link"
+                                href="{{ route('usuarios.formulario') }}">Formulario</a></li>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Formulario 2</a>
+                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Iniciar Sesión</a></li>
+                    @endif
                 </ul>
             </div>
         </div>
@@ -34,6 +56,7 @@
     
 
     <div class="container my-4">
+        {{-- <p>{{Auth::user->nombre}}</p> --}}
         @yield('contenido')
     </div>
 
