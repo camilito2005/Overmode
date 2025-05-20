e23153a813ae99095f53f2ad57700acd889485f4
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-05-19 23:32:06 -0500

    feat:cree una vista en las plantilla para las alertas

--- resources/views/layouts/alertas.blade.php
+++ resources/views/layouts/alertas.blade.php
@@ -4,19 +4,19 @@ if (!isset($type)) {
     $type = 'Info';
 }
     if ($type == "Info") {
-        $class = 'text-blue-800 bg-blue-50 dark:bg-gray-800 dark:text-blue-400';
+        $class = 'text-blue-800 bg-blue-50';
     }
     elseif ($type == "Danger") {
-        $class = 'text-red-800 bg-red-50 dark:bg-gray-800 dark:text-red-400';
+        $class = 'text-red-800 bg-red-50';
     }
     elseif ($type == "Success") {
-        $class = 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400';
+        $class = 'text-green-800 bg-green-50';
     }
     elseif ($type == "Warning") {
-        $class = 'text-yellow-800 bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300';
+        $class = 'text-yellow-800 bg-yellow-50';
     }
     elseif ($type == "Dark") {
-        $class = 'text-gray-800 bg-gray-50 dark:bg-gray-800 dark:text-gray-300';
+        $class = 'text-gray-800 bg-gray-50';
     }
     else {
         $class = '';
