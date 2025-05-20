b1bee9d15f196124ce879c5738b2d65623080599
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-05-19 22:04:05 -0500

    feat: agregue el middleware en estas rutas

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UsuariosController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/' , [UsuariosController::class, 'Index'])->name('index');
 
-Route::get('/usuarios', [UsuariosController::class, 'Form_html'])->name('usuarios.formulario');
-Route::post('/usuarios/Registrar', [UsuariosController::class, 'Registrar'])->name('usuarios.registrar');
-Route::get('/usuarios/listar', [UsuariosController::class, 'Listar'])->name('usuarios.listar');
+Route::get('/', [UsuariosController::class, 'index'])->name('index');
+Route::get('/usuarios/formulario', [UsuariosController::class, 'Form_html'])->name('usuarios.formulario');
+Route::get('/usuarios', [UsuariosController::class, 'Listar'])->name('usuarios.listar');
+Route::post('/usuarios/registrar', [UsuariosController::class, 'Registrar'])->name('usuarios.registrar');
 Route::put('/usuarios/actualizar/{id}', [UsuariosController::class, 'Actualizar'])->name('usuarios.actualizar');
-Route::delete('/usuarios/Eliminar/{id}', [UsuariosController::class, 'Eliminar'])->name('usuarios.eliminar');
+Route::delete('/usuarios/eliminar/{id}', [UsuariosController::class, 'Eliminar'])->name('usuarios.eliminar');
 
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+});
+
+require __DIR__.'/auth.php';
