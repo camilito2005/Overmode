bdd9c343b437a6ba22b6d703d7a84ff9c2cf1eb6
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-07-28 00:32:09 -0500

    style: cambie estilos y agregue cosas de los estilos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -31,6 +31,7 @@
                     @guest
                         <li class="nav-item"><a class="nav-link" href="{{ route('usuarios.formulario') }}">Registrate</a></li>
                         <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Iniciar Sesión</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Contáctanos</a></li>
                     @endguest
 
                     @auth
