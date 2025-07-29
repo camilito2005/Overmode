3a4b5a8444b3e9b45a935e2955118901fba5af80
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-07-28 23:40:19 -0500

    fix: quite el footer del menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -60,9 +60,9 @@
 
 
 
-    <div class="container my-4">
+    {{-- <div class="container my-4"> --}}
         @yield('contenido')
-    </div>
+    {{-- </div> --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
@@ -72,53 +72,7 @@
 
     @stack('js')
 
-{{-- FOOTER --}}
-        <footer class="bg-dark text-light pt-5 pb-4">
-            <div class="container text-md-left">
-                <div class="row text-md-left">
-
-                    <!-- Marca -->
-                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h5 class="text-uppercase mb-4 font-weight-bold">Overmode</h5>
-                        <p>Tu destino de moda en línea. Encuentra tu estilo con nuestras colecciones exclusivas.</p>
-                    </div>
-
-                    <!-- Navegación -->
-                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                        <h5 class="text-uppercase mb-4 font-weight-bold">Navegación</h5>
-                        <p><a href="{{route('index')}}" class="text-light text-decoration-none">Inicio</a></p>
-                        <p><a href="{{ route('catalogo') }}" class="text-light text-decoration-none">Catálogo</a></p>
-                        <p><a href="#quienes-somos" class="text-light text-decoration-none">¿Quiénes somos?</a></p>
-                        <p><a href="#contacto" class="text-light text-decoration-none">Contáctanos</a></p>
-                    </div>
-
-                    <!-- Contacto -->
-                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
-                        <h5 class="text-uppercase mb-4 font-weight-bold">Contacto</h5>
-                        <p><i class="bi bi-house-door me-2"></i> Cartagena, Colombia</p>
-                        <p><i class="bi bi-envelope me-2"></i> contacto@overmode.com</p>
-                        <p><i class="bi bi-telephone me-2"></i> +57 300 123 4567</p>
-                    </div>
-
-                    <!-- Redes sociales -->
-                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mt-3">
-                        <h5 class="text-uppercase mb-4 font-weight-bold">Síguenos</h5>
-                        <a href="#" class="text-light me-3"><i class="bi bi-facebook"></i></a>
-                        <a href="#" class="text-light me-3"><i class="bi bi-instagram"></i></a>
-                        <a href="#" class="text-light me-3"><i class="bi bi-twitter"></i></a>
-                    </div>
-                </div>
-
-                <hr class="my-3">
-
-                <div class="row align-items-center">
-                    <div class="col-md-7 col-lg-8">
-                        <p class="text-center text-md-start">© 2025 Overmode. Todos los derechos reservados.</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-
+    
 </body>
 
 </html>
