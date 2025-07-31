d472f44b49e740e45c49ec1b66a0f3f210568319
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-07-30 23:04:50 -0500

    feat: agregue un icono

--- resources/views/carrito/carrito.blade.php
+++ resources/views/carrito/carrito.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.menu')
 
-@section('titulo', 'Mi Carrito')
+@section('titulo', 'Overmode - Carrito de Compras')
 
 @push('css')
     <meta name="auth" content="{{ Auth::check() ? '1' : '0' }}">
     <meta name="ruta-catalogo" content="{{ route('catalogo') }}">
     <meta name="ruta-vaciar" content="{{ route('carrito.vaciar') }}">
+    <link rel="shortcut icon" href="{{asset('storage/iconos/carro.png')}}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <style>
         .producto-img {
