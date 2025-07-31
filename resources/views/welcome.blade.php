550b31d6cdfa670782cbc3f453ecd56168ea781b
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-07-30 23:11:05 -0500

    feat: agregue un icono

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
 @push('css')
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="shortcut icon" href="{{asset('storage/iconos/hogar.png')}}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
