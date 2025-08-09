8507e0c0a60ac452aae69d467358cead179d390e
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-08-09 13:41:09 -0500

    agregue las relaciones de las subcategorias al modelo de las subcategorias

--- app/Models/SubcategoriasModel.php
+++ app/Models/SubcategoriasModel.php
@@ -8,10 +8,19 @@ class SubcategoriasModel extends Model
 {
     //
     protected $table = 'subcategorias';
-    protected $fillable = ['subcategoria', 'categoria_id'];
+    protected $fillable = ['subcategoria', 'categoria_id', 'parent_id'];
 
     public function categoria()
     {
         return $this->belongsTo(CategoriaModel::class, 'categoria_id');
+    }
+    public function parent()
+    {
+        return $this->belongsTo(SubcategoriasModel::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(SubcategoriasModel::class, 'parent_id');
     }
 }
