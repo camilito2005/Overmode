02da06a5e583a5dc29d804d64a0ec19605648e62
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-08-08 23:56:56 -0500

    feat: agregue un modal para crear las subcategorias

---
+++ resources/views/modales/agregar_subcategorias.blade.php
@@ -0,0 +1,33 @@
+{{-- Modal Agregar Subcategoría --}}
+<div class="modal fade" id="modalAgregarSubcategoria" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Agregar Subcategoría</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('subcategorias.agregar') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">Nombre</label>
+                        <input type="text" name="nombre" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Categoría</label>
+                        <select class="form-select" name="categoria_id" id="categoria_id">
+                            <option value="">Seleccione una categoría</option>
+                            @foreach ($categorias as $categoria)
+                                <option value="{{ $categoria->id }}"
+                                    {{ old('categoria_id') == $categoria->id ? 'selected' : '' }}>
+                                    {{ $categoria->nombre }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-success">Agregar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
