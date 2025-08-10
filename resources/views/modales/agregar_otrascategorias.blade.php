3bf776a6598c3d5319b5078f7692e0049173fde3
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-08-10 15:40:14 -0500

    feat: cree el modal para añadir las subcategorias hijas

---
+++ resources/views/modales/agregar_otrascategorias.blade.php
@@ -0,0 +1,45 @@
+{{-- Modal Agregar Subcategoría --}}
+<div class="modal fade" id="modalAgregarSubSubcategoria" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Agregar Sub-sub-categoría</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ route('subcategorias.agregar') }}" method="POST">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label">Nombre</label>
+                        <input type="text" name="nombre" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Categoría</label>
+                        <select class="form-select" name="categoria_id" id="categoria_id">
+                            <option value="">Seleccione una categoría</option>
+                            @foreach ($categorias as $categoria)
+                                <option value="{{ $categoria->id }}"
+                                    {{ old('categoria_id') == $categoria->id ? 'selected' : '' }}>
+                                    {{ $categoria->nombre }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">SubCategoría</label>
+                        <select class="form-select" name="parent_id" id="categoria_id">
+                            <option value="">Seleccione una subcategoría</option>
+                            @foreach ($subcategorias as $subcategoria)
+                                    <option value="{{ $subcategoria->id }}"
+                                        {{ old('subcategoria_id') == $subcategoria->id ? 'selected' : '' }}>
+                                        {{ $subcategoria->subcategoria }}
+                                    </option>
+                                @endforeach
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-success">Agregar</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
