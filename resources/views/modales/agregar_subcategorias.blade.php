a504715711f40d2e21fc5da45519eac895314027
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-08-10 15:40:43 -0500

    feat: cree el modal para añadir las subcategorias

--- resources/views/modales/agregar_subcategorias.blade.php
+++ resources/views/modales/agregar_subcategorias.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Agregar Subcategoría</h5>
+                <h5 class="modal-title">Agregar Sub-categoría</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
