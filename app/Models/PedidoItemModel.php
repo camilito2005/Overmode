7b0b3a351dc75d9b092de09ff4a5eef7ed0f94cf
camilito2005 <marrugobarrioscamilo2005@gmail.com>
2025-08-19 21:25:42 -0500

    feat: cree el modelo de los item de los pedidos

---
+++ app/Models/PedidoItemModel.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\UsuarioModel;
+use App\Models\Productosmodel;
+use App\Models\Pedido;
+
+class PedidoItemModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pedidos_item';
+
+    protected $fillable = [
+        'pedido_id',
+        'producto_id',
+        'talla_id',
+        'color_id',
+        'cantidad',
+        'precio_unitario',
+        'total'
+    ];
+
+    public function pedido()
+    {
+        return $this->belongsTo(Pedido::class, 'pedido_id');
+    }
+
+    public function usuario()
+    {
+        return $this->belongsTo(UsuarioModel::class, 'usuario_id');
+    }
+
+    public function producto()
+    {
+        return $this->belongsTo(Productosmodel::class, 'producto_id');
+    }
+}
